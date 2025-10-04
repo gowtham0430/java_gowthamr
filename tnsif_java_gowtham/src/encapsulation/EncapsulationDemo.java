c0134a649aec558a2440945b6a11425b43ccc8b5
arjun <arjun@Smiley>
2025-10-04 22:35:19 +0530

    commit

--- tnsif_java_gowtham/src/encapsulation/EncapsulationDemo.java
+++ tnsif_java_gowtham/src/encapsulation/EncapsulationDemo.java
@@ -3,7 +3,11 @@ package encapsulation;
 public class EncapsulationDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Execution obj = new Execution();
+		obj.setSerialNum(101);
+		obj.setName("Pooja");
+		obj.setAge(20);
+		System.out.println(obj);
 
 	}
 
